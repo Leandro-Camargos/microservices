1ac5d3c7f882cf0f7a50daec806edc31b0cb0180
Leandro Camargos <leandronik1984@gmail.com>
2021-10-21 18:45:57 -0300

    projetos

---
+++ book-service/src/main/java/br/com/camargos/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package br.com.camargos.repository;
+
+import br.com.camargos.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
--- greeting-service/src/main/java/br/com/camargos/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/com/camargos/controllers/GreetingController.java
@@ -11,15 +11,16 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController public class GreetingController {
 
-	private static final String template = "%s!, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@Autowired private GreetingConfiguration configuration;
 
-	@RequestMapping("/greeting") public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
 		if(name.isEmpty()){
 			name = configuration.getDefaultValue();
 		}
-		return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting()));
+		return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
 	}
 }
